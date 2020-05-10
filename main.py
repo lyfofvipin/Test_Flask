d0b95917c1a006db03dce6220f39ba5e94f16f5a
Vipin Kumar <vipikuma@redhat.com>
2020-05-10 22:19:51 +0530

    using jinja template via value passing

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", title="Flask | Home")
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title="Flask | About")
     
 if __name__ == "__main__":
     app.run(debug=True)
